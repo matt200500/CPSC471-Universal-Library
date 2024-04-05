b7f4cf35cfa5226d0f6af761a547e6ec4faeb773
Jason Rempel <rempeljason0@gmail.com>
2024-04-05 12:33:03 -0600

    No longer tracking with git many files in  and various folders of . These files don't need to be tracking with git I believe. This was all done through adding a  file. Also there are couple of my initial changes for the browse books page and single book page.

--- frontend/src/components/BrowseBooks.js
+++ frontend/src/components/BrowseBooks.js
@@ -1,4 +1,6 @@
 import React, { Component } from "react";
+import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
 
 export default class BrowseBooks extends Component {
   constructor(props) {
@@ -6,6 +8,22 @@ export default class BrowseBooks extends Component {
   }
 
   render() {
-    return <h1>This is the Browse Books </h1>;
+    return <Grid container align="center">
+        <Grid item align="center" xs={8}>
+          <Typography component="h4" variant="h4">
+            Book 1
+          </Typography>
+        </Grid>
+        <Grid item align="center" xs={8}>
+          <Typography component="h4" variant="h4">
+            Book 2
+          </Typography>
+        </Grid>
+        <Grid item align="center" xs={8}>
+          <Typography component="h4" variant="h4">
+            Book 3
+          </Typography>
+        </Grid>
+      </Grid>;
   }
 }
---
+++ frontend/src/components/SingleBook.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class SingleBook extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return <h1>This is the Single Book page</h1>;
+  }
+}
