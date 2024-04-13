d3ed4355173fff852255498f2e95930aeb4337c6
matt200500 <matteocusanelli@gmail.com>
2024-04-13 04:43:49 -0600

    addidentally overrided my stuff

--- frontend/src/components/LoginPage.js
+++ frontend/src/components/LoginPage.js
@@ -4,45 +4,36 @@ export default class LoginPage extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      userId: "",
-      password: ""
+      user_login: {
+        user_id: "",
+        User_password: "",
+      },
+      UserData: []
     };
   }
 
-  handleInputChange = (e) => {
+  handleInputChange = (event) => {
+    const { name, value } = event.target;
     this.setState({
-      [e.target.name]: e.target.value
+      user_login: {
+        ...this.state.user_login,
+        [name]: value
+      }
     });
   };
 
   handleSubmit = async (e) => {
     e.preventDefault();
-    const { userId, password } = this.state;
-    const response = await fetch('/api/login/', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        user_id: userId,
-        password: password,
-      }),
-    });
-
+    const { user_id, User_password } = this.state.user_login;
+    const response = await fetch(`/api/login?user_id=${user_id}&User_password=${User_password}`);
     if (response.ok) {
       const data = await response.json();
-<<<<<<< HEAD
       console.log(data);
       localStorage.setItem('user_id', data.user_id);
       this.setState({ UserData: data });
       alert("Successfully logged in");
-=======
-      console.log("Login successful", data);
-      localStorage.setItem('user', JSON.stringify(data));
-      this.props.history.push('/');
->>>>>>> 8c7dce8b2c6c42e2c44ee45e9906c523d8c50b82
     } else {
-      alert("Invalid credentials");
+      alert("Failed to login");
     }
   };
 
@@ -64,36 +55,20 @@ export default class LoginPage extends Component {
         <form onSubmit={this.handleSubmit}>
           <input
             type="text"
-<<<<<<< HEAD
             name="user_id"
-=======
-            name="userId"
->>>>>>> 8c7dce8b2c6c42e2c44ee45e9906c523d8c50b82
             placeholder="User ID"
             onChange={this.handleInputChange}
             value={this.state.userId}
           />
           <input
             type="password"
-<<<<<<< HEAD
             name="User_password"
             placeholder="password"
-=======
-            name="password"
-            placeholder="Password"
->>>>>>> 8c7dce8b2c6c42e2c44ee45e9906c523d8c50b82
             onChange={this.handleInputChange}
             value={this.state.password}
           />
           <button type="submit">Login</button>
         </form>
-<<<<<<< HEAD
-=======
-      </div>
-      <div className="signup-stuff">
-        <p>If you do not have an account, you can create one by pressing the button below:</p>
-        <a href="signup">Create Account</a>
->>>>>>> 8c7dce8b2c6c42e2c44ee45e9906c523d8c50b82
       </div>
         <div className="signup-stuff">
           <p>If you do not have an account, you can create one by pressing the button below:</p>
